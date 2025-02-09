d37e7e883c6f39d1bc63c6c355c89d842e6d6f80
Maurice042 <ejikememaurice7@gmail.com>
2025-02-09 18:23:31 +0100

    I hosted on render

--- authAPI/urls.py
+++ authAPI/urls.py
@@ -1,10 +1,22 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("API.urls")),
     path("api/v1/", include("djoser.urls")),
     path("api/v1/", include("djoser.urls.jwt")),
+    path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
+
+
+router = DefaultRouter()
+
+
+urlpatterns += router.urls
