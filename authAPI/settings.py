5730981e38aed466ec407a1c07c6030d66a5bd0d
Junior's Signature <162041570+juniorssignature21@users.noreply.github.com>
2025-02-11 16:08:09 +0100

    Update settings.py

--- authAPI/settings.py
+++ authAPI/settings.py
@@ -156,11 +156,12 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT =
+SIMPLE_JWT ={
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "AUTH_TOKEN_CLASSES":          
+ ("rest_framework_simplejwt.tokens.AccessToken",),
 }
 
 DJOSER = {
